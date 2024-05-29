2a93005e7c276232321914ab34d1f53ed0d2fa76
jasmeetsingh-1 <jasmeets9877@gmail.com>
2024-05-30 00:15:40 +0530

    drag and drop code completed for the project page

--- src/components/projectPage/dragAndDrop.jsx
+++ src/components/projectPage/dragAndDrop.jsx
@@ -3,151 +3,171 @@ import "./dragAndDrop.css";
 import { Draggable, Droppable } from "react-drag-and-drop";
 import TaskContainer from "../reusable/taskContainer";
 
+
+const project_task = {
+  toDo: [
+    {
+      id:1,
+      title: "Create a Visual Style Guide",
+      dueTime: "09.00 AM - 09.30 AM",
+    },
+    {
+      id:2,
+      title: "Local setup Doc",
+      dueTime: "09.00 AM - 10.30 AM",
+    },
+    {
+      id:3,
+      title: "Ui fixes for website",
+      dueTime: "09.00 AM - 09.30 AM",
+    },
+  ],
+  inProgress: [
+    {
+      id:4,
+      title: "Complete environment Setup",
+      dueTime: "09.00 AM - 10.30 AM",
+    },
+  ],
+  completed: [],
+  overdue: [],
+};
+
 function DragDrop() {
-  const project_task = {
-    toDo: [
-      {
-        title: "Create a Visual Style Guide",
-        dueTime: "09.00 AM - 09.30 AM",
-      },
-      {
-        title: "Local setup Doc",
-        dueTime: "09.00 AM - 10.30 AM",
-      },
-      {
-        title: "Ui fixes for website",
-        dueTime: "09.00 AM - 09.30 AM",
-      },
-    ],
-    inProgress: [
-      {
-        title: "Complete environment Setup",
-        dueTime: "09.00 AM - 10.30 AM",
-      },
-    ],
-    completed: [],
-    overdue: [],
-  };
-
-  const [toDoDrop, setToDoDrop] = useState(project_task.toDo);
-    const [inProgressDrop, setinProgressDrop] = useState(project_task.inProgress);
-    const [completedDrop, setcompletedDrop] = useState(project_task.completed);
-    const [overDueDrop, setoverDueDrop] = useState(project_task.overdue);
-
-  const toDoDrophandler = (task) => {
+  const [projectTaskList, setProjectTaskList]= useState(project_task);
+  
+  const taskDropHandler = (task, dropLocation) => {
     const data = JSON.parse(task.task);
-    console.log(data);
-    let prevItems = [...toDoDrop];
-    const index = prevItems.findIndex((item) => item === data.title);
-    if (index !== -1) {
-      //if already in it then move to first index
-      prevItems = prevItems.filter((item) => item !== data.task);
-      prevItems = [data.task, ...prevItems];
-      setToDoDrop(prevItems);
-    } else {
-      setToDoDrop((prev) => {
-        return [...prev, data];
-      });
-    }
-  };
-  const inProgressDrophandler = (task) => {
-    const data = JSON.parse(task.task);
-    console.log(data);
-    let prevItems = [...inProgressDrop];
-    const index = prevItems.findIndex((item) => item === data.title);
-    if (index !== -1) {
-      //if already in it then move to first index
-      prevItems = prevItems.filter((item) => item !== data.task);
-      prevItems = [data.task, ...prevItems];
-      setinProgressDrop(prevItems);
-    } else {
-      setinProgressDrop((prev) => {
-        return [...prev, data];
-      });
-    }
-  };
-  const completedDrophandler = (task) => {
-    const data = JSON.parse(task.task);
-    console.log(data);
-    let prevItems = [...completedDrop];
-    const index = prevItems.findIndex((item) => item === data.title);
-    if (index !== -1) {
-      //if already in it then move to first index
-      prevItems = prevItems.filter((item) => item !== data.task);
-      prevItems = [data.task, ...prevItems];
-      setcompletedDrop(prevItems);
-    } else {
-      setcompletedDrop((prev) => {
-        return [...prev, data];
-      });
-    }
-  };
-  const overDueDrophandler = (task) => {
-    const data = JSON.parse(task.task);
-    console.log(data);
-    let prevItems = [...overDueDrop];
-    const index = prevItems.findIndex((item) => item === data.title);
-    if (index !== -1) {
-      //if already in it then move to first index
-      prevItems = prevItems.filter((item) => item !== data.task);
-      prevItems = [data.task, ...prevItems];
-      setoverDueDrop(prevItems);
-    } else {
-      setoverDueDrop((prev) => {
-        return [...prev, data];
-      });
-    }
-  };
+    const taskItem=data.item; 
+   let prevList;
 
+   if (data.pickLocation === "toDo") {
+    prevList = [...projectTaskList.toDo];
+    prevList = prevList.filter((item) => item.id !== taskItem.id);
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        toDo: prevList,
+      };
+    });
+  }
+  
+  if (data.pickLocation === "inProgress") {
+    prevList = [...projectTaskList.inProgress];
+    prevList = prevList.filter((item) => item.id !== taskItem.id);
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        inProgress: prevList,
+      };
+    });
+  }
+  
+  if (data.pickLocation === "completed") {
+    prevList = [...projectTaskList.completed];
+    prevList = prevList.filter((item) => item.id !== taskItem.id);
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        completed: prevList,
+      };
+    });
+  }
+  
+  if (data.pickLocation === "overdue") {
+    prevList = [...projectTaskList.overdue];
+    prevList = prevList.filter((item) => item.id !== taskItem.id);
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        overdue: prevList,
+      };
+    });
+  }
+  
+  if (dropLocation === "toDo") {
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        toDo: [...prev.toDo, taskItem],
+      };
+    });
+  }
+  
+  if (dropLocation === "inProgress") {
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        inProgress: [...prev.inProgress, taskItem],
+      };
+    });
+  }
+  
+  if (dropLocation === "completed") {
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        completed: [...prev.completed, taskItem],
+      };
+    });
+  }
+  
+  if (dropLocation === "overdue") {
+    setProjectTaskList((prev) => {
+      return {
+        ...prev,
+        overdue: [...prev.overdue, taskItem],
+      };
+    });
+  }
+  
 
+  };
+ 
   return (
     <div className="drag-drop-holder">
       <div className="drop-sections">
         <p>To Do</p>
-        <Droppable onDrop={toDoDrophandler} types={["task"]}>
-          {
-            toDoDrop?.map((item)=> (
-              <Draggable type="task" data={JSON.stringify(item)}>
-              <TaskContainer taskDetails={item}/>
-            </Draggable>
-            ))
-          }
+        <Droppable onDrop={(task)=>taskDropHandler(task,"toDo")} types={["task"]}>
+          {projectTaskList.toDo?.map((item,index) => {
+            const task={item:item,pickLocation:"toDo"};
+            return <Draggable type="task" data={JSON.stringify(task)} key={index}>
+              <TaskContainer taskDetails={item} />
+            </Draggable>;
+          })}
         </Droppable>
       </div>
       <div className="drop-sections">
         <p>In Progress </p>
-        <Droppable onDrop={inProgressDrophandler} types={["task"]}>
-        {
-            inProgressDrop?.map((item)=> (
-              <Draggable type="task" data={JSON.stringify(item)}>
-              <TaskContainer taskDetails={item}/>
-            </Draggable>
-            ))
-          }
+        <Droppable onDrop={(task)=>taskDropHandler(task,"inProgress")} types={["task"]}>
+          {projectTaskList.inProgress?.map((item,index) => {
+            const task={item:item,pickLocation:"inProgress"};
+            return <Draggable type="task" data={JSON.stringify(task)} key={index}>
+              <TaskContainer taskDetails={item} />
+            </Draggable>;
+          })}
         </Droppable>
       </div>
       <div className="drop-sections">
         <p>Completed </p>
-        <Droppable onDrop={completedDrophandler} types={["task"]}>
-        {
-            completedDrop?.map((item)=> (
-              <Draggable type="task" data={JSON.stringify(item)}>
-              <TaskContainer taskDetails={item}/>
-            </Draggable>
-            ))
-          }
+        <Droppable onDrop={(task)=>taskDropHandler(task,"completed")} types={["task"]}>
+          {projectTaskList.completed?.map((item,index) => {
+            const task={item:item,pickLocation:"completed"};
+            return <Draggable type="task" data={JSON.stringify(task)} key={index}>
+              <TaskContainer taskDetails={item} />
+            </Draggable>;
+          })}
         </Droppable>
       </div>
       <div className="drop-sections">
-        <p>Overdue </p>
-        <Droppable onDrop={overDueDrophandler} types={["task"]}>
-        {
-            overDueDrop?.map((item)=> (
-              <Draggable type="task" data={JSON.stringify(item)}>
-              <TaskContainer taskDetails={item}/>
-            </Draggable>
-            ))
-          }
+        <p>Overdue</p>
+        <Droppable onDrop={(task)=>taskDropHandler(task,"overdue")} types={["task"]}>
+          {projectTaskList.overdue?.map((item,index) => {
+            const task={item:item,pickLocation:"overdue"};
+            return <Draggable type="task" data={JSON.stringify(task)} key={index}>
+              <TaskContainer taskDetails={item} />
+            </Draggable>;
+          })}
         </Droppable>
       </div>
     </div>
