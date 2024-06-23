60d013fcd1f681d211db64ef543deaedd6f8be5e
jasmeetsingh-1 <jasmeets9877@gmail.com>
2024-06-23 22:27:36 +0530

    warning fixes

--- src/components/pages/project.jsx
+++ src/components/pages/project.jsx
@@ -41,7 +41,8 @@ function ProjectPage() {
                 <path d="M20.3 18.8c-.8 0-1.5-.7-1.5-1.5v-5.9c0-.8.7-1.5 1.5-1.5s1.5.7 1.5 1.5v5.9c-.1.8-.7 1.5-1.5 1.5m23.4 0c-.8 0-1.5-.7-1.5-1.5v-5.9c0-.8.7-1.5 1.5-1.5s1.5.7 1.5 1.5v5.9c0 .8-.7 1.5-1.5 1.5M24.7 12.9h14.6v3H24.7zM12.9 21.7h38.2v3H12.9zM45.2 27.6h2.9v2.9h-2.9zM39.3 27.6h3v2.9h-3zM33.5 27.6h2.9v2.9h-2.9zM27.6 27.6h2.9v2.9h-2.9zM21.7 27.6h3v2.9h-3zM45.2 33.5h2.9v2.9h-2.9zM39.3 33.5h3v2.9h-3zM33.5 33.5h2.9v2.9h-2.9zM27.6 33.5h2.9v2.9h-2.9zM21.7 33.5h3v2.9h-3zM15.9 33.5h2.9v2.9h-2.9zM45.2 39.3h2.9v3h-2.9zM39.3 39.3h3v3h-3zM33.5 39.3h2.9v3h-2.9zM27.6 39.3h2.9v3h-2.9zM21.7 39.3h3v3h-3zM15.9 39.3h2.9v3h-2.9zM39.3 45.2h3v2.9h-3zM33.5 45.2h2.9v2.9h-2.9zM27.6 45.2h2.9v2.9h-2.9zM21.7 45.2h3v2.9h-3zM15.9 45.2h2.9v2.9h-2.9z"></path>
               </g>
             </svg>
-            12 Feb, 2023
+            12 Feb, 2023 
+            {/* project date */}
           </section>
         </div>
 
@@ -49,44 +50,39 @@ function ProjectPage() {
 
         <div className="project-metaData-section">
           <table>
-            <tr className="priority">
-              <td>Priority</td>
-              <td style={{width:"52px"}}>Medium</td>
-            </tr>
-            <div></div>
-            <tr className="dueDate">
-              <td>Due Date</td>
-              <td> 28 Feb 2023 </td>
-            </tr>
-            <div></div>
-            <tr className="tags">
-              <td>Tags</td>
-              <td className="tags-list">
-              {tagData.map((item,index) => (<span key={index} style={{backgroundColor:`${item.colorCode}`}}>{item.tagTitle}</span>))}
-              </td>
-            </tr>
-            <div></div>
-            <tr className="assignees">
-              <td>Assignees</td>
-              <td>
-                <div>
-                  <span>🧔🏻‍♂️</span>
-                  Irvan Moses
-                </div>
-                <div>
-                  <span>🧔🏻‍♂️</span>
-                  Irvan Moses
-                </div>
-                <div>
-                  <span>🧔🏻‍♂️</span>
-                  Irvan Moses
-                </div>
-                {/* <div className="showMore-assignees">
-                  +
-                </div> */}
-              </td>
-            </tr>
-            <div></div>
+            <tbody>
+              <tr className="priority">
+                <td>Priority</td>
+                <td style={{width:"52px"}}>Medium</td>
+              </tr>
+              <tr className="dueDate">
+                <td>Due Date</td>
+                <td> 28 Feb 2023 </td>
+              </tr>
+              <tr className="tags">
+                <td>Tags</td>
+                <td className="tags-list">
+                {tagData.map((item,index) => (<span key={index} style={{backgroundColor:`${item.colorCode}`}}>{item.tagTitle}</span>))}
+                </td>
+              </tr>
+              <tr className="assignees">
+                <td>Assignees</td>
+                <td>
+                  <div>
+                    <span>🧔🏻‍♂️</span>
+                    Irvan Moses
+                  </div>
+                  <div>
+                    <span>🧔🏻‍♂️</span>
+                    Irvan Moses
+                  </div>
+                  <div>
+                    <span>🧔🏻‍♂️</span>
+                    Irvan Moses
+                  </div>
+                </td>
+              </tr>
+            </tbody>
           </table>
 
         </div>
